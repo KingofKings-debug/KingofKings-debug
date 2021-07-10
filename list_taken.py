05b44482e287b83717365591d149305028ef5569
KingofKings-debug <71443601+KingofKings-debug@users.noreply.github.com>
2021-07-10 14:35:51 +0530

    Delete list_taken.py

--- list_taken.py
+++
@@ -1,2 +0,0 @@
-name=['youtube', 'redit', 'insta', 'whatsapp', 'classroom']
-network=['https://www.youtube.com/', 'https://www.reddit.com/', 'https://www.instagram.com/', 'https://web.whatsapp.com/', 'https://classroom.google.com/u/0/h']
